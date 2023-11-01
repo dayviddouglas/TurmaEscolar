364c57d9652799d47278b608531a2c3bac036392
dayviddouglas <dayvidacademico@gmail.com>
2023-11-01 17:16:09 -0300

    Ajusting  main text of Turma.

--- frontend/turmaEscolar/src/pages/Turmas.jsx
+++ frontend/turmaEscolar/src/pages/Turmas.jsx
@@ -1,11 +1,15 @@
 import Header from "../components/Header";
+import {Text, Box} from '@chakra-ui/react'
 
 const Turmas = () => {
   return (
     <div>
          <Header/>
-
-         <h1> Turmas </h1>
+         <Box mt={10}w={280} h={50}  mx="auto">
+         <Text fontSize='2xl' as='b'>
+            Turmas:
+         </Text>
+         </Box>
     </div>
   )
 }
